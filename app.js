e854aa1293aef3d3edea964a17a30a7c5cf7a1bf
Michelle Tran <mptran0101@berkeley.edu>
2020-05-09 10:35:55 -0700

    added moment.js

--- app.js
+++ app.js
@@ -1,26 +1,66 @@
 $(document).ready(function () {
-    var lat;
-    var long;
+    var $currentDate = $("#currentDate");
+    var $currentTemp = $("#currentTemp");
+    var $currentCity = $("#currentCity");
+    var $weatherIcon = $("#weatherIcon");
+    var $weatherStats = $("#weatherStats");
 
-    navigator.geolocation.getCurrentPosition(initLocation);
+    var searchCity;
+    var temp;
+    var currentIcon;
+    var description;
+    var hourly = [];
+    var sevenDay = [];
+    var sevenDayIcon = [];
+    var date;
+
+    //get today's date
 
     //get current location
-    function initLocation(position) {
-        lat = position.coords.latitude;
-        long = position.coords.longitude;
-        console.log(lat);
-        console.log(long);
-        getCurrentWeather(lat, long);
-    }   
+    $("input").on("keydown",function search(e) {
+        if(e.keyCode == 13) {
+            e.preventDefault();
+            searchCity = $(this).val();
+            $currentCity.html(`<h1>${searchCity}</h1>`)
+            getCurrentWeather(searchCity);
+        }
+    });
 
-    function getCurrentWeather(lat, long) {
+    function getCurrentWeather(city) {
         $.ajax({
             type: "GET",
-            url: `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${long}&exclude={part}&appid=c64d3ff6b9a9c58c72a6b260cf9dc8e7`,
+            url: `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=c64d3ff6b9a9c58c72a6b260cf9dc8e7`,
             data: 'json',
         }).then(function (res) {
             console.log(res);
+            temp = res.main.feels_like;
+            currentIcon = res.weather[0].icon;
+            description = res.weather[0].description;
+            currentTemp(temp);
         });
     }
-    
+
+    //init current temp
+    function currentTemp(temp) {
+        temp = Math.floor(convertFahrenheit(temp));
+        renderWeather();
+    }
+
+    //convert to F
+    function convertFahrenheit(temp) {
+        temp = ((temp - 273.15) * 1.8) + 32;
+        return temp;
+    }
+
+    //convert to C
+    function convertCelsius(temp) {
+        temp = temp - 273.15;
+        return temp;
+    }
+
+    //render current weather
+    function renderWeather() {
+        $currentTemp.html(`<h1>${temp}\u00B0F</h1><p>${description}</p>`);
+        $weatherIcon.html(`<img src="http://openweathermap.org/img/wn/${currentIcon}@2x.png" alt="weather icon">`);
+    }
 });
